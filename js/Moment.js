77c920f6b7b64d3c8cc87d52289dd3fb38a29ad5
Your Name <you@example.com>
2020-04-23 22:50:09 +0200

    Fixing url bug

--- js/Moment.js
+++ js/Moment.js
@@ -29,8 +29,8 @@ class Moment {
     changebackground() {
         const { today } = this;
 
-        const dayImages = "images/day";
-        const nightImages = "images/night";
+        const dayImages = location.href + "images/day";
+        const nightImages = location.href + "momentum/images/night";
 
         const random = Math.floor(Math.random() * this.imageCount + 1);
 
