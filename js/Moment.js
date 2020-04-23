56594c071462085f74a935a909675b4072f4d3d8
Your Name <you@example.com>
2020-04-24 00:08:58 +0200

    Fixes

--- js/Moment.js
+++ js/Moment.js
@@ -19,7 +19,7 @@ class Moment {
         const hour = today.getHours();
         if (hour < 12 && hour > 6) {
             return "Good morning";
-        } else if (hour < 19) {
+        } else if (hour < 19 && hour > 12) {
             return "Good evening";
         } else {
             return "Good night";
